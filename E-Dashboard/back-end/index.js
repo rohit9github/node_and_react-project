66903e773a37cc5d31617070476c7e9af87ffa49
gohil rohit <gohilrohit309@gmail.com>
2024-06-14 21:09:44 +0530

    mongodb and mongoose install

--- E-Dashboard/back-end/index.js
+++ E-Dashboard/back-end/index.js
@@ -1,4 +1,23 @@
-const express = require('express')
+// const {MongoClient} = require("mongodb");
+// const url = "mongodb://localhost:27017";
+// const client = new MongoClient(url)
+//  async function getData(){
+//     let result = await client.connect();
+//    let db = result.db("e-com");
+//    let collection = db.collection("products");
+//    console.log(collection.find({}).toArray);
+// }
+
+const express = require('express');
+const mongoose = require("mongoose")
+const connect= async ()=>{
+    mongoose.connect("mongodb://localhost:27017/e-com");
+    const productSchema = new mongoose.Schema({});
+    const products = mongoose.model("product",productSchema)
+    const data = await products.find();
+    console.log(data);
+}
+connect
 const app = express()
 const port = 9010
 app.get('/', (req, res) => res.send('Hello World!'))
